c464c779abe0a274ff386c3d9d04fc5c36bd258d
Cassiano Junior <cassianojuniorww@gmail.com>
2022-11-03 18:44:45 -0300

    :sparkles: Configuring OAuth2

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode } from 'react';
+import { GOOGLE_AUTH_CLIENT_ID } from '@env';
+import * as AuthSession from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -7,6 +13,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -17,18 +24,47 @@ interface AuthProviderProps {
 }
 
 export const AuthContextProvider = ({ children }: AuthProviderProps) => {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+
+  const [req, res, promptAsync] = Google.useAuthRequest({
+    clientId: GOOGLE_AUTH_CLIENT_ID,
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email'],
+  });
+
   const signIn = async () => {
-    console.log('entrou');
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (err) {
+      console.log(err);
+      throw err;
+    } finally {
+      setIsUserLoading(false);
+    }
   };
+
+  const signInWithGoogle = async (accessToken: string) => {
+    console.log('Auth token ->', accessToken);
+  };
+
+  useEffect(() => {
+    const responseWithSuccess = res?.type === 'success';
+    const hasAccessToken =
+      responseWithSuccess && res.authentication?.accessToken;
+    if (hasAccessToken) {
+      const accessToken = res.authentication.accessToken;
+      signInWithGoogle(accessToken);
+    }
+  }, [res]);
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: 'Cassiano Junior',
-          avatarUrl: 'https://github.com/CassianoJunior',
-        },
+        isUserLoading,
+        user,
       }}
     >
       {children}
